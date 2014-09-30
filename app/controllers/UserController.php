aecd7411aff9a40a62d2c6c146996dbe46518682
tisca26 <gerry.t26@gmail.com>
2014-09-30 16:34:50 -0500

    Agrega edición de usuario con Fb

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -86,7 +86,11 @@ class UserController extends ProtectedController {
         $user->nombre = Input::get('nombre');
         $user->apellidos = Input::get('apellidos');
         $user->username = Input::get('username');
-        $user->password = (Input::get('password') == '')? $user->password : Hash::make(Input::get('password'));
+        if(is_numeric($user->username)){
+            $user->password = Hash::make('4CC3S4C0NFb');
+        }else{
+            $user->password = (Input::get('password') == '')? $user->password : Hash::make(Input::get('password'));
+        }        
         $user->estatus = Input::get('estatus', '0');
         $user->role_id = Input::get('role_id');
         $user->save();
